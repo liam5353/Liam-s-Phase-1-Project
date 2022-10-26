741d5ce57241f2491f1fa12f420de01f261c958f
“Liam <okeefe.liam@yahoo.com>
2022-10-26 01:08:24 -0500

    event listener

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const right = document.getElementById("right")               // setting a const element from DOM
-const left = document.getElementById("left")
-const todos = document.getElementById("todos");              // gets todo div
-const textBoxes = document.getElementsByTagName("input");    // textbox div
-const todo =                                                 // creates todo string
+const right = document.getElementById("right")             // gets elements from the dom
+const left = document.getElementById("left")               // document.getElementsByTagName
+const todos = document.getElementById("todos");            // will find every element with the selected tag name
+const textBoxes = document.getElementsByTagName("input");  // and store them to the named variable in an array style
+
+const todo =                                               // creates a blank todo object
 {
     id: "",
     title:  "",
@@ -10,11 +11,35 @@ const todo =                                                 // creates todo str
     imageURL:"",
     notes:""
 }
-// todolist = []
 
-function renderTodos(todoList) {                                       // creates first set of todos
-    for (let i = 0; i < todoList.length; i++) {                        // empty string for adding new todos for textbox
-        let addTodo = "<br></br>";                                        
+function getAllTodos() {                                       
+    fetch('http://localhost:3000/todoList/')              // fetch request - async
+    .then(res => res.json())                              
+    .then(data => renderTodos(data))                                             
+}
+
+getAllTodos()                                             
+
+let form = document.getElementById("btn")
+let values = []                                        //Create blank array 
+
+window.onload = function() {                           //When the website loads
+    for(let i = 0; i < textBoxes.length; i++) {        //for loop to add items from textboxes to array
+        values.push(textBoxes[i]);
+        textBoxes[i].addEventListener("mouseover", function(){ // 1st event listener "mouseover"
+            if(textBoxes[i].value.includes(textBoxes[i].id))
+            {
+                textBoxes[i].value = ""
+            } 
+        })
+    }
+}
+/*
+Needs to be a regular for loop because for each function does not work on document.getElementsByTagName
+*/
+function renderTodos(todoList) {                            // creates initial set of todos
+    for (let i = 0; i < todoList.length; i++) {                        
+        let addTodo = "<br></br>";                          //create placeholder for inital set of todos                                     
         addTodo += "ID: "+todoList[i].id+"<br><br>";
         addTodo += "Title: "+todoList[i].title+"<br><br>";
         addTodo += "Category: "+todoList[i].category+"<br><br>";
@@ -26,7 +51,7 @@ function renderTodos(todoList) {                                       // create
         img.setAttribute("id","img")
         todos.innerHTML += addTodo+"<br><br><br>";
         todos.appendChild(img)
-        document.getElementById("img").addEventListener("click",function() {            // 1st event listener "click"
+        document.getElementById("img").addEventListener("click",function() {  // 1st event listener "click"         
             document.getElementById("img").setAttribute("height","500px");
             document.getElementById("img").setAttribute("width","500px");
             document.getElementById("img").addEventListener("mouseout",function() {     // 2nd event listener "mouseout" to return pic to size
@@ -38,64 +63,36 @@ function renderTodos(todoList) {                                       // create
     return todoList
 }
 
-function getAllTodos() {                                       
-    fetch('http://localhost:3000/todoList/')              // fetch request - async
-    .then(res => res.json())                              // .json to get data - async promise to do something when gets data back
-    .then(data => renderTodos(data))                      // came through so we can grab from string                         
-}
+form.addEventListener("click", (event)=> {   
+    {  
+        values.forEach(function(i){
+            if (i.id == "id")
+            {
+                todo.id = i.value 
+                i.value  = ""
+            } 
+            else if(i.id == "title")
+            {
+                todo.title = i.value 
+                i.value  = ""
+            } 
+            else if(i.id == "category")
+            {
+                todo.category = i.value 
+                i.value  = ""
+            }  
+            else if(i.id == "notes")
+            {
+                todo.notes = i.value 
+                i.value  = ""
+            }  
+        })
+    }   
 
-getAllTodos()                                             
-
-form.addEventListener("submit", (e)=> {                               
-    for(let i = 0; i < textBoxes.length; i++) { 
-        if (i == 0)
-        {
-            todo.id = textBoxes[i].value 
-            textBoxes[i].value  = ""
-        } 
-        else if(i == 1)
-        {
-            todo.title = textBoxes[i].value 
-            textBoxes[i].value  = ""
-        } 
-        else if(i == 2)
-        {
-            todo.category = textBoxes[i].value 
-            textBoxes[i].value  = ""
-        } 
-        else if(i == 3)
-        {
-            todo.imageURL = textBoxes[i].value 
-            textBoxes[i].value  = ""
-        } 
-        else if(i == 4)
-        {
-            todo.notes = textBoxes[i].value 
-            textBoxes[i].value  = ""
-        }  
-    }  
-
-    let addTodo = "";                                        // creates a string using todo object 
+    let addTodo = "";                                        // takes element and adds to div
         addTodo += "<br>ID: "+todo.id+"<br><br>";
         addTodo += "Title: "+todo.title+"<br><br>";
         addTodo += "Category: "+todo.category+"<br><br>";
-        addTodo += "Notes: "+todo.notes+"<br><br>";             
-        let img = document.createElement("img");
-        img.src = todo.imageURL;
-        img.setAttribute("height","200px");
-        img.setAttribute("width","200px");
-        todos.innerHTML += addTodo+"<br><br><br>";
-        todos.appendChild(img)                              // takes element and adds to div
-        e.preventDefault()
+        addTodo += "Notes: "+todo.notes+"<br><br>";  
+        todos.innerHTML += addTodo+"<br><br><br>";                      
 })   
-
-function getTextBoxes() { 
-    for(let i = 0; i < textBoxes.length; i++) { 
-        textBoxes[i].addEventListener("", function() {     // 3rd event listener "mouseover"
-            text = textBoxes[i].value 
-            textBoxes[i].value  = ""
-        })
-    }
-}
-
-getTextBoxes()
